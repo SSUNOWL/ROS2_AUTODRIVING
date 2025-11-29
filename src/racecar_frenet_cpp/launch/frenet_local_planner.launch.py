a11b008ee1dc5e7bc345d3befb7b58ef49259e7d
jungbin906 <jungbin906>
2025-11-30 00:13:26 +0900

    2차

--- src/racecar_frenet_cpp/launch/frenet_local_planner.launch.py
+++ src/racecar_frenet_cpp/launch/frenet_local_planner.launch.py
@@ -3,9 +3,14 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    # 패키지 공유 디렉토리 경로를 가져옴
+    package_share_directory = get_package_share_directory('racecar_frenet_cpp')
+
     return LaunchDescription([
+        # 런치 파라미터 선언
         DeclareLaunchArgument('max_speed', default_value='5.0', description='Maximum speed'),
         DeclareLaunchArgument('max_accel', default_value='2.0', description='Maximum acceleration'),
         DeclareLaunchArgument('max_curvature', default_value='1.0', description='Maximum curvature'),
@@ -17,9 +22,7 @@ def generate_launch_description():
             executable='frenet_local_planner',
             name='frenet_local_planner',
             output='screen',
-            parameters=[os.path.join(
-                get_package_share_directory('racecar_frenet_cpp'), 'config', 'params.yaml'
-            )],
+            parameters=[os.path.join(package_share_directory, 'config', 'params.yaml')],
             remappings=[('/scan', '/robot/scan'), ('/ego_racecar/odom', '/robot/odom')]
         )
     ])
